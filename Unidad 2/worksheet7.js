7ec61595944a1bf81ba3d4e6d481830782524fa7
JairoEM <loseloth@gmail.com>
2018-10-03 13:03:11 +0200

    Improvements on W7

--- Unidad 2/worksheet7.js
+++ Unidad 2/worksheet7.js
@@ -14,10 +14,18 @@ function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
-var map;
-function initMap() {
+// Watch Position
+
+function successW(posM) {
+    let coordinates = posM.coords;
+
+    console.log('Your current position is, until you move:');
+    console.log(`Latitude : ${coordinates.latitude}`);
+    console.log(`Longitude: ${coordinates.longitude}`);
+    console.log(`More or less ${coordinates.accuracy} meters.`);
+    
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 37.1512198, lng: -3.6167244999999997},
+        center: {lat: coordinates.latitude, lng: coordinates.longitude},
         zoom: 16
     });
 
@@ -26,17 +34,61 @@ function initMap() {
     map.addListener('mousemove', function(event) {
         console.log(coordsDiv = 'lat: ' + event.latLng.lat() + ', lng: ' + event.latLng.lng());
     });
-        
-    var myLatLng = {lat: 37.1512198, lng: -3.6167244999999997};
 
-
+    var myLatLng = {lat: 37.1484704, lng: -3.6167965};
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'MI CHABOLO'
+        title: 'My House'
     });
 
-}
+    var positionNow = {lat: coordinates.latitude, lng: coordinates.longitude};
+    var marker = new google.maps.Marker({
+        position: positionNow,
+        map: map,
+        title: 'My Position'
+    });
+
+    var line = new google.maps.Polyline({
+        path: [
+        new google.maps.LatLng(coordinates.latitude, coordinates.longitude),
+        new google.maps.LatLng(37.1484704, -3.6167965)
+        ],
+        strokeColor: "#FF0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 10,
+        map: map
+    });
+};
+
+function mapCharged(){
+    var move = navigator.geolocation.watchPosition(successW, error, [options]);
+};
+
+
+
+// var map;
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: {lat: 37.1512198, lng: -3.6167244999999997},
+//         zoom: 16
+//     });
+
+//     var coordsDiv = document.getElementById('coords');
+//     map.controls[google.maps.ControlPosition.TOP_CENTER].push(coordsDiv);
+//     map.addListener('mousemove', function(event) {
+//         console.log(coordsDiv = 'lat: ' + event.latLng.lat() + ', lng: ' + event.latLng.lng());
+//     });
+        
+//     var myLatLng = {lat: 37.1512198, lng: -3.6167244999999997};
+
+//     var marker = new google.maps.Marker({
+//         position: myLatLng,
+//         map: map,
+//         title: 'MI CHABOLO'
+//     });
+
+// }
 
 // Current Position
 
@@ -70,37 +122,3 @@ function initMap() {
 // }
 
 // var locat = navigator.geolocation.getCurrentPosition(success, error, [options]);
-
-
-// Watch Position
-
-function successW(posM) {
-    let coordinates = posM.coords;
-
-    console.log('Your current position is, until you move:');
-    console.log(`Latitude : ${coordinates.latitude}`);
-    console.log(`Longitude: ${coordinates.longitude}`);
-    console.log(`More or less ${coordinates.accuracy} meters.`);
-    
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: coordinates.latitude, lng: coordinates.longitude},
-        zoom: 16
-    });
-
-    var coordsDiv = document.getElementById('coords');
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(coordsDiv);
-    map.addListener('mousemove', function(event) {
-        console.log(coordsDiv = 'lat: ' + event.latLng.lat() + ', lng: ' + event.latLng.lng());
-    });
-
-    var myLatLng = {lat: 37.1484704, lng: -3.6167965};
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'MI CHABOLO'
-    });
-};
-
-function mapCharged(){
-    var move = navigator.geolocation.watchPosition(successW, error, [options]);
-}
