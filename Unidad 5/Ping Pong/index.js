58e0b02d5462a14dbdaf911c1e73c5b6b3744c07
Jairo <loseloth@gmail.com>
2018-11-05 19:58:31 +0100

    Ping pong, players can move

---
+++ Unidad 5/Ping Pong/index.js
@@ -0,0 +1,93 @@
+function main(){
+    //Declare variables, such as ball, board or players.
+    var ball = document.getElementById("ball");
+    var table = document.getElementById("table");
+    var speedX = 1;
+    var speedY = 1;
+    var x;
+    var y;
+    var player1 = document.getElementById("player1");
+    var player2 = document.getElementById("player2");
+    var move = 6;
+    var positionBlue;
+    var positionRed;
+    var key;
+
+    document.addEventListener('keydown', function move1(event){
+        //Capture the key event
+        key = event.keyCode;
+
+        //Player blue moves
+        positionBlue = parseInt(player1.getAttribute("y"));
+        if(key == 87){
+            if(positionBlue >= 0){
+                player1.setAttribute("y", (positionBlue - move));
+            }
+        }
+        if(key == 83){
+            if(positionBlue <= 170){
+                player1.setAttribute("y", (positionBlue + move));
+            } 
+        }
+
+        //Player red moves
+        positionRed = parseInt(player2.getAttribute("y"));
+        if(key == 38){
+            if(positionRed >= 0){
+                player2.setAttribute("y", (positionRed - move));
+            }
+        }
+        if(key == 40){
+            if(positionRed <= 170){
+                player2.setAttribute("y", (positionRed + move));
+            } 
+        }
+    });
+
+    //Ball hit the right or left walls
+    function moveX(){
+        if(x >= table.getAttribute("width")-10){
+            speedX = speedX * (-1);
+        }
+        if(x <= 10){
+            speedX = Math.abs(speedX);
+        } 
+        ball.setAttribute("cx", (x + speedX));
+    }
+
+    //Ball hit the top or bottom walls
+    function moveY(){
+        if(y >= table.getAttribute("height")-10){
+            speedY = speedY * (-1);
+        }
+        if(y <= 10){
+            speedY = Math.abs(speedY);
+        }
+        ball.setAttribute("cy", (y + speedY));
+    }
+
+    setInterval( () => {
+        x = parseInt(ball.getAttribute("cx"));
+        y = parseInt(ball.getAttribute("cy"));
+        moveY();
+        moveX();
+    }, 5);
+}
+
+
+// position1 = document.getElementById("player1").getAttribute("x");
+// if(event.keyCode == 87){
+//     player1.setAttribute("x", (position1 + 100));
+//     //DUDA IMPORTANTE, POR QUÉ SE PONE RETURN FALSE?!
+// }
+
+
+// document.addEventListener('keydown', function move1(event){
+//     key1 = event.keyCode;
+// });
+// function movePlayer1(){
+//     if(key1 == 87){
+//         player1.setAttribute("x", (player1.getAttribute("x") + 100));
+//     }
+// }
+// movePlayer1;
